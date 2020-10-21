6af4aeddbcf9f4893b2ed921de8feac97805382b
Gui Guy <jedandeganwang1999@gmail.com>
2020-10-21 13:53:42 -0500

    Initial commit

---
+++ expandable.js
@@ -0,0 +1,39 @@
+try {
+    var arr = document.getElementsByClassName("expandable");
+    for(var idx = 0; idx < arr.length; idx++) {
+        // alert("starting " + idx);
+
+        arr[idx].addEventListener("click", function() {
+            // alert(this + " clicked")
+
+            try {
+                // console.log("clicked " + idx);
+                var content;
+                if(this.getAttribute("data-content")) {
+                    content = document.getElementById(this.getAttribute("data-content"));
+                } else {
+                    content = this.nextElementSibling;
+                }
+                // alert(content + " " + content.style.display);
+                if (content.style.display == "none") {
+                    this.setAttribute("data-sign", "-");
+                    content.style.display = null;
+                } else {
+                    this.setAttribute("data-sign", "+");
+                    content.style.display = "none";
+                }
+            } catch(e) {
+                alert(e);
+            }
+
+            return false;
+        }, false);
+
+        if(!arr[idx].getAttribute("data-sign"))
+            arr[idx].setAttribute("data-sign", "+");
+
+        // alert("done " + idx);
+    }
+} catch(err) {
+    alert(err);
+}
